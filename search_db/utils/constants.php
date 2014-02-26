d29d688cbe30cc0fba8d0172381643333dc4a849
chetex <chetex69@gmail.com>
2014-02-26 16:36:51 +0100

    Update constants.php
    Typing private password and user

--- search_db/utils/constants.php
+++ search_db/utils/constants.php
@@ -4,9 +4,9 @@
 // LOCAL
 define ('SERVER', 'localhost:3306' );
 //define ('USER', 'wordpressuser790' );
-define ('USER', 'root' );
+define ('USER', 'xxxx' );
 //define ('PASS', 'oJq.XKcecqn8' );
-define ('PASS', 'admin' );
-define ('DATABASE', 'wordpress790' );
+define ('PASS', 'xxxx' );
+define ('DATABASE', 'xxxxxx' );
 
 ?>
