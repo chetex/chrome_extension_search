59ba276cbf8177e3fd47339740d789f5a7d8a615
nacho <chetex69@gmail.com>
2018-07-13 13:35:19 +0200

    Update constants.php

--- search_db/utils/constants.php
+++ search_db/utils/constants.php
@@ -3,10 +3,6 @@
 // define ('SERVER', 'xx.xx.xx.xx:3306' );
 // LOCAL
 define ('SERVER', 'localhost:3306' );
-//define ('USER', 'wordpressuser790' );
 define ('USER', 'xxxx' );
-//define ('PASS', 'oJq.XKcecqn8' );
 define ('PASS', 'xxxx' );
 define ('DATABASE', 'xxxxxx' );
-
-?>
